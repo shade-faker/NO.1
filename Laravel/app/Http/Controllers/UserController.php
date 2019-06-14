3c52296a31a00afa99736db07c2ffe4d730bf56d
shade-faker <13368161924@163.com>
2019-06-14 23:58:56 +0800

    shl-php-5

---
+++ Laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Save;
+
+class UserController extends Controller
+{
+
+    public function show(Request &$request){
+
+    $name = $request->input('name');
+    $content = $request->input('content');
+        if ($name) {
+
+            $information = new Save;
+
+            $information->name = $name;
+            $information->content = $content;
+
+            $information->save();
+
+                return redirect('/');
+
+        }
+        $users = DB::table('Save')
+            ->where('name', $name)
+            ->get();
+        return view('name', ['name'=> $name]);
+    }
+
+
+}
---
+++ Laravel/app/Save.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Save extends Model
+{
+
+    protected $table = 'information';
+}
